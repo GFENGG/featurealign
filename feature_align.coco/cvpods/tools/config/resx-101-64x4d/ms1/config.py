e55dd03863a269c2d286a103e1943ad076a7b2a1
feng <2482648525@qq.com>
2022-06-04 12:26:50 +0800

    Initial Commit

---
+++ feature_align.coco/cvpods/tools/config/resx-101-64x4d/ms1/config.py
@@ -0,0 +1,72 @@
+import os.path as osp
+
+from cvpods.configs.fcos_config import FCOSConfig
+
+_config_dict = dict(
+    MODEL=dict(
+        WEIGHTS="detectron2://ImageNetPretrained/FBResNeXt/X-101-64x4d.pkl",
+            RESNETS=dict(
+                DEPTH=101,
+                NUM_GROUPS=64,
+                WIDTH_PER_GROUP=4,
+                STRIDE_IN_1X1=False,
+            ),
+        FCOS=dict(
+            PRIOR_PROB=0.02,
+            NORM_REG_TARGETS=True,
+            NMS_THRESH_TEST=0.6,
+            BBOX_REG_WEIGHTS=(1.0, 1.0, 1.0, 1.0),
+            FOCAL_LOSS_GAMMA=2.0,
+            FOCAL_LOSS_ALPHA=0.25,
+            IOU_LOSS_TYPE="giou",
+            REG_WEIGHT=5.0,
+        ),
+    ),
+    DATASETS=dict(
+        TRAIN=("coco_2017_train",),
+        TEST=("coco_2017_test-dev",), # coco_2017_val coco_2017_test-dev
+    ),
+    SOLVER=dict(
+        IMS_PER_BATCH=16,
+        IMS_PER_DEVICE=4,
+        CHECKPOINT_PERIOD=10000,
+        LR_SCHEDULER=dict(
+            STEPS=(120000,160000),
+            MAX_ITER=180000,
+        ),
+        OPTIMIZER=dict(
+            BASE_LR=0.01,
+        ),
+    ),
+    #480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800, 832, 864, 896, 928, 960
+    #640, 672, 704, 736, 768, 800
+    #480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800
+    INPUT=dict(
+        AUG=dict(
+            TRAIN_PIPELINES=[
+                ("ResizeShortestEdge", dict(
+                    short_edge_length=(480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800), max_size=1333, sample_style="choice")),
+                ("RandomFlip", dict()),
+            ],
+            TEST_PIPELINES=[
+                ("ResizeShortestEdge", dict(
+                    short_edge_length=800, max_size=1333, sample_style="choice")),
+            ],
+        )
+    ),
+    TEST=dict(
+        EVAL_PERIOD=10000,
+    ),
+    OUTPUT_DIR=osp.join(
+        '/data/Outputs/model_logs/cvpods_playground',
+        osp.split(osp.realpath(__file__))[0].split("playground/")[-1]),
+)
+
+
+class AutoAssignConfig(FCOSConfig):
+    def __init__(self):
+        super(AutoAssignConfig, self).__init__()
+        self._register_configuration(_config_dict)
+
+
+config = AutoAssignConfig()
